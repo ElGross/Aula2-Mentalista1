fc26cf9550572768ff1b8771054a0871db9f698c
ElGross <151382540+ElGross@users.noreply.github.com>
2023-11-18 12:49:24 -0300

    Jogo adivinhação
    Adivinhar número entre uma determinada sequência.

---
+++ script.js
@@ -0,0 +1,3 @@
+var numeroSecreto = 581;
+var palpite = prompt('Escolha um número entre 1 e 1000');
+if(palpite == numeroSecreto) {alert('Wow! Você está com sorte!')} else{alert('Não foi dessa vez. Tente nocamente!')}; //se o palpite for correto ou não
